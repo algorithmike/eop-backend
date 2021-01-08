11277d6c2e0c62b2da9288f6199ed608272fc5cb
Michael Fernandez <mikevfern@gmail.com>
2021-01-08 13:32:13 -0800

    Created dummyData.js for dummy data.

---
+++ src/dummyData.js
@@ -0,0 +1,61 @@
+export const dummyUsers = [
+    {
+        id: 'abcd1234',
+        username: 'algorithmike',
+        email: 'mike@test.com',
+        password: 'password',
+        realname: 'Michael',
+        description: 'Lorem ipsum...',
+    },
+    {
+        id: 'efgh5678',
+        username: 'justanothausa',
+        email: 'testuser@test.com',
+        password: 'passwordTest',
+        realname: 'Test Name',
+        description: 'This is a test user!'
+    },
+    {
+        id: 'ijkl9101112',
+        username: 'imarobot',
+        email: 'robot@matrix.com',
+        password: 'robotPassword',
+        realname: 'Mr. Smith',
+    }
+];
+
+export const dummyContent = [
+    {
+        id: 'contentID123',
+        mediaType: 'IMAGE',
+        title: 'The First Dummy Image',
+        postedAt: 20201225,
+        postedBy: 'abcd1234',
+        postedFromEop: false,
+        media: 'http://dummycontent1.png',
+        mediaPreview: 'http://dummycontentpreview1.png',
+        description: 'This is some dummy content. Gotta figure out where to store those images.'
+    },
+    {
+        id: 'contentID456',
+        mediaType: 'IMAGE',
+        title: 'The Second Dummy Image',
+        postedAt: 11112020,
+        postedBy: 'efgh5678',
+        postedFromEop: false,
+        media: 'http://dummycontent2.png',
+        mediaPreview: 'http://dummycontentpreview2.png',
+        description: 'There\'s gotta be at least two dummy content.'
+    },
+    {
+        id: 'contentID789',
+        mediaType: 'IMAGE',
+        title: 'The Third Dummy Image',
+        postedAt: 20210108,
+        postedBy: 'abcd1234',
+        postedFromEop: false,
+        media: 'http://dummycontent3.png',
+        mediaPreview: 'http://dummycontentpreview3.png',
+        description: 'Three is a good number.'
+    }
+];
--- src/index.js
+++ src/index.js
@@ -1,15 +1,64 @@
 import { GraphQLServer } from 'graphql-yoga'
+import { dummyUsers, dummyContent } from './dummyData'
 
+// Next:
+// Add content: [Content!]! to User.
+// add postedBy: User! to Content.
+// create Event type, query, resolver.
+// add event: Event! to Content.
 const typeDefs = `
   type Query {
-    hello(name: String): String!
+    users: [User!]!
+    content: [Content!]!
+  }
+
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    password: String!
+    realname: String
+    description: String
+    profilePic: String
+    content: [Content!]!
+  }
+
+  type Content {
+    id: ID!
+    mediaType: String!
+    title: String!
+    postedAt: Int!
+    postedBy: User!
+    postedFromEop: Boolean!
+    media: String!
+    mediaPreview: String!
+    description: String!
   }
 `
 
 const resolvers = {
   Query: {
-    hello: (_, { name }) => `Hello ${name || 'World!'}`,
+    users(){
+      return dummyUsers
+    },
+    content(){
+      return dummyContent
+    }
   },
+  User: {
+    content(parent){
+      return dummyContent.filter(contentItem => {
+        return contentItem.postedBy === parent.id
+      })
+    }
+  },
+  Content: {
+    postedBy(parent){
+      return dummyUsers.find(user => {
+        return user.id === parent.postedBy
+      })
+    }
+  }
 }
 
 const server = new GraphQLServer({ typeDefs, resolvers })
