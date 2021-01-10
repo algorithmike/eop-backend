1f7e088dd18babb33d92d0a81eeda01bc95fa0d8
Michael Fernandez <mikevfern@gmail.com>
2021-01-09 19:11:03 -0800

    Added createContent Mutation definition, and resolver

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # eop-backend
+
+### Nice to haves, but not of immediate priority:
+* Ability to create future events, not yet associated with any content. Currently, events are only created as objects of data that define a time and location around a content item's creation.
--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 import { GraphQLServer } from 'graphql-yoga'
+import gql from 'graphql-tag'
+import { nanoid } from 'nanoid'
 import { dummyUsers, dummyContent, dummyEvents } from './dummyData'
 
+
 // Next:
-// add attendees: [User!]! to Event.
-const typeDefs = `
+// add event creation to the createContent Mutation
+// createUser Mutation
+// editUser Mutation
+// editContent Mutation
+// editEvent Mutation; Only events created from own Content creation.
+const typeDefs = gql`
   type Query {
-    users: [User!]!
-    content: [Content!]!
-    events: [Event!]!
+    users(text: String): [User!]!
+    content(text: String): [Content!]!
+    events(text: String): [Event!]!
+  }
+
+  type Mutation {
+    createContent(
+      mediaType: String!
+      title: String!
+      description: String!
+      postedFromEop: Boolean!
+      postedBy: String!
+      _event_id: ID
+      _event_title: String
+      _event_coordinates: String
+      _event_description: String
+      _event_country: String
+      _event_state: String
+      _event_city: String
+      _event_landmark: String
+    ): Content!
   }
 
   type User {
@@ -52,14 +77,55 @@ const typeDefs = `
 
 const resolvers = {
   Query: {
-    users(){
+    users(_,{text}){
+      if(text){
+        return dummyUsers.filter(user => {
+          return (
+            user.username.toLocaleLowerCase().includes(text.toLocaleLowerCase()) ||
+            user.email.toLocaleLowerCase().includes(text.toLocaleLowerCase()) ||
+            user.realname.toLocaleLowerCase().includes(text.toLocaleLowerCase())
+          )
+        })
+      }
       return dummyUsers
     },
-    content(){
+    content(_, {text}){
+      if(text){
+        return dummyContent.filter(item => {
+          return (
+            item.title.toLocaleLowerCase().includes(text.toLocaleLowerCase()) ||
+            item.description.toLocaleLowerCase().includes(text.toLocaleLowerCase())
+          )
+        })
+      }
       return dummyContent
     },
-    events(){
+    events(_, {text}){
+      if(text){
+        return dummyEvents.filter(event => {
+          return (
+            event.title.toLocaleLowerCase().includes(text.toLocaleLowerCase()) ||
+            event.description.toLocaleLowerCase().includes(text.toLocaleLowerCase()) ||
+            event.landmark.toLocaleLowerCase().includes(text.toLocaleLowerCase())
+          )
+        })
+      }
       return dummyEvents
+    }
+  },
+  Mutation: {
+    createContent(_, args){
+      const { mediaType, title, description, postedFromEop, postedBy, _event_id } = args
+      const newContent = { mediaType, title, description, postedFromEop, postedBy } 
+      
+      if(!newContent.event){
+        newContent.event = nanoid()
+
+        // Create new event here
+        
+      }
+      dummyContent.push(newContent)
+      return newContent
     }
   },
   User: {
