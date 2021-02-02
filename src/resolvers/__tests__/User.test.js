cac9534db40e74b477a7002ceaedb0367adc46e5
Michael Fernandez <mikevfern@gmail.com>
2021-02-01 20:33:13 -0800

    User.test.js setup. Add testing dev dependencies

--- src/resolvers/__tests__/User.test.js
+++ src/resolvers/__tests__/User.test.js
@@ -1,4 +1,27 @@
-test('This is a test.', () => {
-    expect(true).toBe(true)
+import 'regenerator-runtime/runtime.js'
+import 'cross-fetch/polyfill'
+import ApolloClient, { gql } from 'apollo-boost';
 
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql'
+});
+
+describe('Query', () => {
+    test('all users.', async () => {
+        const queryUsers = gql`
+            query {
+                content {
+                    id
+                    title
+                    mediaType
+                    mediaUrl
+                    mediaPreviewUrl
+                    description
+                }
+            }
+        `
+        await client.query({
+            query: queryUsers
+        })
+    })
 })
