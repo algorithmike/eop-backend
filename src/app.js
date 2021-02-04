73a3baabce16ce2dd0b66d481e519f7c7b53c951
Michael Fernandez <mikevfern@gmail.com>
2021-02-04 08:01:57 -0800

    .

--- src/app.js
+++ src/app.js
@@ -40,6 +40,13 @@ app.use(
   })
 )
 
+// Cors
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const server = new ApolloServer({
   typeDefs,
   resolvers: {
