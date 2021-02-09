fc5e808b460789b368b16457424d230ed6de6b78
Michael Fernandez <mikevfern@gmail.com>
2021-02-09 10:07:58 -0800

    seed events and data

--- src/resolvers/Mutations/__tests__/User.test.js
+++ src/resolvers/Mutations/__tests__/User.test.js
@@ -17,6 +17,7 @@ beforeAll( async () => {
     await prisma.event.deleteMany()
     await prisma.user.deleteMany()
 
+    // Seed users.
     const {id: user1_id} = await prisma.user.create({
         data: {
             email: 'user.test.one1@email.com',
@@ -36,6 +37,95 @@ beforeAll( async () => {
             realname: 'User Test 2',
             description: 'This is the second user tester.',
             profilePicUrl: 'https://i.pinimg.com/564x/4d/f1/93/4df193b6dc700bd806fc0273506f8587.jpg'
+        }
+    })
+
+    // Seed events.
+    const {id: event1_id} = await prisma.event.create({
+        data: {
+            organizer: {
+                connect: {
+                    id: user1_id
+                }
+            },
+            coordinates: '123, 456',
+            title: 'Event One Title',
+            description: 'This is a description for the first event.',
+            country: 'USA',
+            state: 'NV',
+            city: 'Reno',
+            landmark: 'Circus Circus'
+        }
+    })
+    const {id: event2_id} = await prisma.event.create({
+        data: {
+            organizer: {
+                connect: {
+                    id: user2_id
+                }
+            },
+            coordinates: '987, 654',
+            title: 'Event Two Title',
+            description: 'This is a description for the second event.',
+            country: 'Canada',
+            city: 'Vancouver',
+        }
+    })
+
+    const {id: content1_id} = await prisma.content.create({
+        data: {
+            mediaType: 'image',
+            mediaUrl: 'https://avatarfiles.alphacoders.com/124/thumb-1920-124420.jpg',
+            mediaPreviewUrl: 'https://avatarfiles.alphacoders.com/124/thumb-1920-124420.jpg',
+            title: 'Test Content One',
+            author: {
+                connect: {
+                    id: user1_id
+                }
+            },
+            event: {
+                connect: {
+                    id: event1_id
+                }
+            },
+        }
+    })
+
+    const {id: content2_id} = await prisma.content.create({
+        data: {
+            mediaType: 'image',
+            mediaUrl: 'https://avatarfiles.alphacoders.com/124/thumb-1920-124420.jpg',
+            mediaPreviewUrl: 'https://avatarfiles.alphacoders.com/124/thumb-1920-124420.jpg',
+            title: 'Test Content Two',
+            author: {
+                connect: {
+                    id: user2_id
+                }
+            },
+            event: {
+                connect: {
+                    id: event2_id
+                }
+            },
+        }
+    })
+
+    const {id: content3_id} = await prisma.content.create({
+        data: {
+            mediaType: 'image',
+            mediaUrl: 'https://avatarfiles.alphacoders.com/124/thumb-1920-124420.jpg',
+            mediaPreviewUrl: 'https://avatarfiles.alphacoders.com/124/thumb-1920-124420.jpg',
+            title: 'Test Content Three',
+            author: {
+                connect: {
+                    id: user2_id
+                }
+            },
+            event: {
+                connect: {
+                    id: event1_id
+                }
+            },
         }
     })
 })
