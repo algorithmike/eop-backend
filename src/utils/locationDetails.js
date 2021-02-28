c768880aecf2b2c265a27360e850083f0167914b
Michael Fernandez <mikevfern@gmail.com>
2021-02-28 10:48:09 -0800

    set up proper return value for getLocFromCoords()

--- src/utils/locationDetails.js
+++ src/utils/locationDetails.js
@@ -4,11 +4,16 @@ import axios from 'axios';
 // /geocoding/v5/{endpoint}/{longitude},{latitude}.json
 
 const getLocFromCoords = async (latitude, longitude) => {
-    const response = await axios.get(
+    const {data} = await axios.get(
         `https://api.mapbox.com/geocoding/v5/mapbox.places/
         ${longitude},${latitude}.json?access_token=${process.env.MAPBOX_KEY}`
     )
-    return response.data
+
+    let landmark = data.features[0].text
+    let [streetAddress] = data.features[0].place_name.split(',')
+    let [city, state, country] = data.features[2].place_name.split(',').map(item => item.trim())
+
+    return {country, state, city, streetAddress, landmark}
 }
 
 export default getLocFromCoords;
